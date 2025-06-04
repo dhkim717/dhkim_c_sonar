933d5db7ee42af2aaba75ae51d5499807e085f36
Donghoon <dhkim717@gmail.com>
2025-06-04 09:59:02 -0500

    no buffer overflow

---
+++ snprintf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char source[] = "Hello, GitHub!";
+    char destination[50];  // Make sure the destination has enough space
+
+    // Copy the string safely using snprintf
+    snprintf(destination, sizeof(destination), "%s", source);
+
+    // Get the length of the copied string
+    int length = strlen(destination);
+
+    // Print the results
+    printf("Source string: %s\n", source);
+    printf("Copied string: %s\n", destination);
+    printf("Length of copied string: %d\n", length);
+
+    return 0;
+}
+
